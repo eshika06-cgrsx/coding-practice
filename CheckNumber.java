35f944dda0eb0bdda4dd7c06118ece9a04e3eb77
Eshika <emwork011@gmail.com>
2026-01-08 21:56:49 +0530

    Day 2:CheckNumber program in Java

---
+++ CheckNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class LeapYear
+{
+
+	public static void main (String [] args) 
+{
+
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number:");
+int year=sc.nextInt();
+
+if((year%400==0)| |(year%4==0&&year%100!=0))
+{
+System.out.println("It is a leap year");
+}
+else
+{
+System.out.println("It isn't leap year")
+}
+}
+}
+//Day 2 practice
