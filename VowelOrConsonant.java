dbbf07dfc4ea4ed3b2c8b5bd3e850eee114959a2
Eshika <emwork011@gmail.com>
2026-01-19 20:09:07 +0530

    day 4 vowel or consonant program

---
+++ VowelOrConsonant.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class VowelOrConsonant {
+ public static void main(String[] args)
+{
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Enter a character: ");
+ char ch = sc.next().charAt(0);
+ if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+{
+ if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+ ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+ {
+ System.out.println(ch + " is a Vowel");
+}
+else
+{
+ System.out.println(ch + " is a Consonant");
+ }
+ }
+else
+ {
+ System.out.println("Not an alphabet");
+ }
+ sc.close();
+}
+}
